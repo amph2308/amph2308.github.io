89146e537a316abcb19a79dd521e1853b2482bce
amph2308 <167256582+amph2308@users.noreply.github.com>
2024-10-30 21:10:03 -0600

    in class assignment 10

---
+++ js/wk_10.js
@@ -0,0 +1,10 @@
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+  }
+
+document.getElementById("paragraph").style.color = "orange";
+
+document.getElementById("vector").addEventListener("click", function(){
+    alert("You just got Vectored... AGAIN!");
+});
